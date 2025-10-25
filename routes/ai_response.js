72cb01c1ea6633ea68ff8a1e98a2591a3f79df80
psylg5 <psylg5@nottingham.ac.uk>
2025-10-25 13:18:22 +0100

    added two chat messages

--- routes/ai_response.js
+++ routes/ai_response.js
@@ -3,10 +3,42 @@ const { GoogleGenAI } = require("@google/genai");
 class GeminiModel {
     constructor() {
         this.geminiAI = new GoogleGenAI({});
+        this.chat = this.geminiAI.chats.create({
+            model: "gemini-2.5-flash",
+            history: [
+                {
+                    role: "user",
+                    parts: [ { text: "Hello "}],
+                },
+                {
+                    role: "model",
+                    parts: [ { text: "Great to meet you" }],
+                },
+            ],
+        });
     }
 
     async generateContent(prompt, ws) {
         try {
+
+            const stream1 = await this.chat.sendMessageStream({
+                message: "I have 2 dogs in my house",
+            })
+
+            for await (const chunk of stream1) {
+                console.log(chunk.text);
+                console.log("_".repeat(80));
+            }
+
+            const stream2 = await this.chat.sendMessageStream({
+                message: "How many paws are in my house?",
+            });
+
+            for await (const chunk of stream2) {
+                console.log(chunk.text);
+                console.log("_".repeat(80));
+            }
+
             const result = await this.geminiAI.models.generateContentStream({
                 model: "gemini-2.5-flash",
                 contents: prompt
